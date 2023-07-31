910a9250ef7b39dbc949f4f3e7eab49c1a91c8e1
byeongjuPark <bottlepark28@gmail.com>
2023-07-31 22:16:49 +0900

    first commit

---
+++ src/main/java/com/example/study_springboot_chat/StudySpringbootChatApplication.java
@@ -0,0 +1,14 @@
+package com.example.study_springboot_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpringbootChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudySpringbootChatApplication.class, args);
+	}
+	// zzz
+
+}
